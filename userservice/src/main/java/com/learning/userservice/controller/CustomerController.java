422cd1c6de2b44d75ac72b81a56363ea1c6830b7
AmanGohite <86658855+AmanGohite@users.noreply.github.com>
2021-07-13 17:47:28 +0530

    first commit for userservice

---
+++ userservice/src/main/java/com/learning/userservice/controller/CustomerController.java
@@ -0,0 +1,71 @@
+package com.learning.userservice.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.learning.userservice.exception.CustomerNotFoundException;
+import com.learning.userservice.model.Customer;
+import com.learning.userservice.model.CustomerLogin;
+import com.learning.userservice.model.Customers;
+import com.learning.userservice.model.GenericAPIResponse;
+import com.learning.userservice.model.Orders;
+import com.learning.userservice.model.PlaceOrder;
+import com.learning.userservice.service.CustomerService;
+
+@RestController
+@RequestMapping(value="/customer")
+public class CustomerController{
+	
+	@Autowired
+	CustomerService custService;
+	
+	@PostMapping(value="/login")
+	public ResponseEntity<Customer> loginCustomer(@RequestBody @Valid CustomerLogin login){
+		Customer resp = custService.loginCustomer(login);
+		if (resp == null) {
+			throw new CustomerNotFoundException("email id or password is incorrect");
+		}
+		return new ResponseEntity<Customer>(resp, HttpStatus.OK);
+
+		
+	}
+	
+	@PostMapping(value="/register")
+	public ResponseEntity<GenericAPIResponse> addCustomer(@RequestBody @Valid Customers customer){
+		GenericAPIResponse resp = custService.addCustomer(customer);
+		return new ResponseEntity<GenericAPIResponse>(resp,HttpStatus.CREATED);
+	}
+	
+	@PostMapping(value="/order")
+	public ResponseEntity<GenericAPIResponse> placeOrder(@RequestBody PlaceOrder placeOrder){
+		GenericAPIResponse resp = custService.placeOrder(placeOrder);
+		return new ResponseEntity<GenericAPIResponse>(resp,HttpStatus.OK);
+	}
+		
+	@GetMapping(value="/{custId}")
+	public ResponseEntity<List<Orders>> getCustomerOrder(@PathVariable("custId") Integer custId){
+		return new ResponseEntity<List<Orders>>(custService.getCustomerOrders(custId),HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{custId}/order/duration/{month}")
+	public ResponseEntity<List<Orders>> getPastOrdersByMonth(@PathVariable("custId")Integer custId,@PathVariable("month") Integer month){
+		return new ResponseEntity<List<Orders>>(custService.getPastOrdersByMonth(custId,month),HttpStatus.OK);
+	}
+
+}
---
+++ userservice/src/main/java/com/learning/userservice/model/Product.java
@@ -0,0 +1,58 @@
+package com.learning.userservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class Product {
+	
+	
+	private Integer productId;
+	private String name;
+	private String seller;
+	private Double price;
+	
+	
+	public Product() {}
+	
+	
+	
+	public Product(Integer productId, String name, String seller, Double price) {
+		super();
+		this.productId = productId;
+		this.name = name;
+		this.seller = seller;
+		this.price = price;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSeller() {
+		return seller;
+	}
+	public void setSeller(String seller) {
+		this.seller = seller;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	
+	
+	
+}
